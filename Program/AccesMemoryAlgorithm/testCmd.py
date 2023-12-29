c30b83f671f04acdc0d4aeca0e9c5a104f633efc
Maximus Nolte <maximus.nolte@online.de>
2023-12-29 23:53:19 +0100

    Der scheiß geht jetzt!

--- Program/AccesMemoryAlgorithm/testCmd.py
+++ Program/AccesMemoryAlgorithm/testCmd.py
@@ -1,3 +1,5 @@
+import os
+import sys
 from subprocess import Popen, PIPE, CalledProcessError
 import process_interface
 from time import sleep
@@ -8,26 +10,50 @@ from ctypes import *
 #235 3
 #231 4
 secondSearchVal = 241
+thirdSearchVal = 235
 
 firstSearch = []
 secondSearch = []
+thirdSearch = []
 
-with Popen("memory_scanner.exe", stdout=PIPE, bufsize=1, universal_newlines=True) as p:
-    for line in p.stdout:
-        firstSearch.append(line.removeprefix("'").removesuffix("\n").strip())
-if p.returncode != 0:
-    raise CalledProcessError(p.returncode, p.args)
+def fetchFaders():
+    with Popen("memory_scanner.exe", stdout=PIPE, bufsize=1, universal_newlines=True) as p:
+        for line in p.stdout:
+            firstSearch.append(line.removeprefix("'").removesuffix("\n").strip())
+    if p.returncode != 0:
+        raise CalledProcessError(p.returncode, p.args)
 
-print("Finished 1. Search!")
-sleep(10)
+    print("Finished 1. Search!")
+    sleep(5)
 
-process = process_interface.ProcessInterface()
-process.open("Daslight4")
+    process = process_interface.ProcessInterface()
+    process.open("Daslight4")
 
-for address in firstSearch:
-    int_val = c_int.from_buffer(process.read_memory(int(address), buffer_size=8))
-    print(int_val.value)
-    print("kek")
+    for address in firstSearch:
+        int_val = c_int.from_buffer(process.read_memory(int(address), buffer_size=8))
+        if int_val.value == secondSearchVal:
+            secondSearch.append(address)
 
-print("Finished 2. Search!")
-print(secondSearch)
+    print("Finished 2. Search!")
+    print(secondSearch)
+    sleep(5)
+
+    for address2 in secondSearch:
+        int_val2 = c_int.from_buffer(process.read_memory(int(address2), buffer_size=8))
+        if int_val2.value == thirdSearchVal:
+            thirdSearch.append(address2)
+
+
+
+    print("Finished 3. Search!")
+    print(thirdSearch)
+
+if __name__ == '__main__':
+    try:
+        fetchFaders()
+    except KeyboardInterrupt:
+        print('Interrupted by Keyboard')
+        try:
+            sys.exit(130)
+        except SystemExit:
+            os._exit(130)
