16c67e2eff31acec505bec76d882b7cba6ed5a23
Maximus Nolte <maximus.nolte@online.de>
2023-12-29 23:06:36 +0100

    Testing

--- Program/AccesMemoryAlgorithm/testCmd.py
+++ Program/AccesMemoryAlgorithm/testCmd.py
@@ -1,16 +1,33 @@
 from subprocess import Popen, PIPE, CalledProcessError
-output = []
+import process_interface
+from time import sleep
+import ctypes
 
-with Popen("", stdout=PIPE, bufsize=1, universal_newlines=True) as p:
-    for line in p.stdout:
-        output.append(line.removeprefix("'").removesuffix("\n").strip())
-
-if p.returncode != 0:
-    raise CalledProcessError(p.returncode, p.args)
-
-print(output)
-
-#246 1
+#246 1 <-- is in EXE implemented!
 #241 2
 #235 3
 #231 4
+secondSearchVal = 241
+
+firstSearch = []
+secondSearch = []
+
+with Popen("memory_scanner.exe", stdout=PIPE, bufsize=1, universal_newlines=True) as p:
+    for line in p.stdout:
+        firstSearch.append(line.removeprefix("'").removesuffix("\n").strip())
+if p.returncode != 0:
+    raise CalledProcessError(p.returncode, p.args)
+
+print("Finished 1. Search!")
+sleep(10)
+
+Process = process_interface.ProcessInterface()
+Process.open("Daslight4")
+
+for address in firstSearch:
+    intvalue = ctypes.c_int.from_buffer(Process.read_memory(address, 4))
+    if intvalue == secondSearchVal:
+        secondSearch.append(address)
+
+print("Finished 2. Search!")
+print(secondSearch)
