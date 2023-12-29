654a2f9331065b5d010e15d4d8fe08bceea35ba1
Maximus Nolte <maximus.nolte@online.de>
2023-12-29 22:16:19 +0100

    Testing

--- Program/Serial Access/testCmd.py
+++ Program/Serial Access/testCmd.py
@@ -1,6 +1,10 @@
-import subprocess
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, CalledProcessError
 
-process = Popen(["main.exe"], stdout=subprocess.PIPE)
-while True:
-    print(process.stdout.readline())
+outut = []
+
+with Popen("test.exe", stdout=PIPE, bufsize=1, universal_newlines=True) as p:
+    for line in p.stdout:
+        output.append(line.removeprefix("'").removesuffix("\n").strip())
+
+if p.returncode != 0:
+    raise CalledProcessError(p.returncode, p.args)
